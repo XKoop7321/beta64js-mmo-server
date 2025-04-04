890f72f38ea944a033141d57c8d1f3977553701a
uuphoria2 <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-04-04 16:24:00 -0500

    minor change

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ echo "using git hash $GIT_HASH"
 docker login
 
 docker buildx build -t beta64js .
-docker tag sm64jsarchive gmanthemarioguy/beta64js:latest
+docker tag beta64js gmanthemarioguy/beta64js:latest
 docker push gmanthemarioguy/beta64js:latest
 docker tag beta64js gmanthemarioguy/beta64js:$GIT_HASH
 docker push gmanthemarioguy/beta64js:$GIT_HASH
